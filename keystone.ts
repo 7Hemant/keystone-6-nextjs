c1759f0ee48b717f304e50d7defeaf006ab8b1b8
7hemant <hemantmourya127@gmail.com>
2023-05-23 14:46:12 +0530

    server cors

--- keystone.ts
+++ keystone.ts
@@ -4,9 +4,9 @@ import { seedDemoData } from "./src/keystone/seed";
 import type { Context } from ".keystone/types";
 
 export default config({
-  server: {
-    cors: { origin: ["http://localhost:4000"], credentials: true },
-  },
+  // server: {
+  //   cors: { origin: ["http://localhost:4000"], credentials: true },
+  // },
   db: {
     provider: "sqlite",
     url: `file:${process.cwd()}/keystone.db`, // next.js requires an absolute path for sqlite
